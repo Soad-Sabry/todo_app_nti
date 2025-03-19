57b94ecd4c615ba784c704dde8bf35f5c5497296
DESKTOP <soaadsabry81@gmail.com>
2025-03-19 22:02:50 +0200

    first

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,52 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "changePassword": MessageLookupByLibrary.simpleMessage("تغيير الباسورد"),
+    "description": MessageLookupByLibrary.simpleMessage("الوصف"),
+    "email": MessageLookupByLibrary.simpleMessage("الاميل"),
+    "haveAccount": MessageLookupByLibrary.simpleMessage("تمتلك حساب بالفعل"),
+    "hintTextTitle": MessageLookupByLibrary.simpleMessage("العنوان"),
+    "hintUserName": MessageLookupByLibrary.simpleMessage("اسم المستخدم"),
+    "letsStart": MessageLookupByLibrary.simpleMessage("هيا بنا نبدا"),
+    "login": MessageLookupByLibrary.simpleMessage("تسجيل دخول"),
+    "name": MessageLookupByLibrary.simpleMessage("الاسم"),
+    "newPassword": MessageLookupByLibrary.simpleMessage("الباسورد الجديد"),
+    "noHaveAccount": MessageLookupByLibrary.simpleMessage("لا امتلك اكونت"),
+    "noTasks": MessageLookupByLibrary.simpleMessage(
+      "لا يوجد تاسكات \n اضغط على الزار لاضافه تاسك جديد",
+    ),
+    "oldPassword": MessageLookupByLibrary.simpleMessage("الباسورد القديم "),
+    "password": MessageLookupByLibrary.simpleMessage("الباسوورد"),
+    "passwordConfirm": MessageLookupByLibrary.simpleMessage("تاكيد كلمه السر "),
+    "register": MessageLookupByLibrary.simpleMessage("انشاء حساب"),
+    "save": MessageLookupByLibrary.simpleMessage("حفظ"),
+    "setting": MessageLookupByLibrary.simpleMessage("الاعدادات "),
+    "titleAddTask": MessageLookupByLibrary.simpleMessage("اضافه تاسك"),
+    "titleOnBoarding": MessageLookupByLibrary.simpleMessage(
+      "هل أنت مستعد لإنجاز مهامك؟ لنقم بذلك معًا.",
+    ),
+    "updateProfile": MessageLookupByLibrary.simpleMessage("تحديث البروفايل"),
+    "welcome": MessageLookupByLibrary.simpleMessage("مرحبا بكم"),
+  };
+}
